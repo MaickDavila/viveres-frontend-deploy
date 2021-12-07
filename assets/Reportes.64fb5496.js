dca6673affd8a33448dd09f6f6b398160b2ead38
MaickDavila <librajunior13@gmail.com>
2021-12-07 09:36:00 -0500

    con load

---
+++ assets/Reportes.64fb5496.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./app.a9edc286.js";import{o as e,d as a}from"./vendor.f30c4ba5.js";var o=r({},[["render",function(r,o){return e(),a("h1",null,"Reportes View")}]]);export{o as default};
